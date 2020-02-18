f4a3d6484b36595b8e6e057ce967d152c681362b
KadyrovAmir <amir.kadyrov.97@mail.ru>
2020-02-18 21:03:08 +0300

    added beta for "memes_only" mode (without db)

--- bot.py
+++ bot.py
@@ -13,6 +13,8 @@ reddit = praw.Reddit(client_id=env('REDDIT_CLIENT_ID'),
                      user_agent='Created by Mirzik')
 image_link_check = re.compile(r'^https://(i\.imgur\.com|i\.redd\.it)/.+')
 reddit_gif_formats = ['.gif', 'gifv']
+memes_only = {}
+meme_subreddits = ['youngpeopleyoutube', 'Tinder']
 
 
 def reddit_random_post():
@@ -29,7 +31,11 @@ def reddit_random_post():
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, {}.\nВоспользуйся командой /next, чтобы получить новый пост с Реддита!'.format(message.from_user.first_name))
+    if message.from_user.id not in memes_only:
+        memes_only[message.from_user.id] = False
+    bot.send_message(message.chat.id,
+                     'Привет, {}.\nВоспользуйся командой /next, чтобы получить новый пост с Реддита!'.format(
+                         message.from_user.first_name))
 
 
 @bot.message_handler(commands=['echo'])
@@ -48,6 +54,44 @@ def new_post_from_reddit(message):
         bot.send_photo(message.chat.id,
                        post['url'],
                        caption='{} (from /r/{})'.format(post['title'], post['subreddit']))
+
+
+@bot.message_handler(commands=['enable_memes'])
+def get_only_memes(message):
+    print(message.from_user.id)
+    if memes_only[message.from_user.id]:
+        memes_only[message.from_user.id] = False
+        text_message = 'Режимом мемов деактивирован!\n Теперь ты будешь получать случайные посты со всего Реддита.'
+    else:
+        memes_only[message.from_user.id] = True
+        text_message = 'Режимом мемов активирован!\n Теперь ты будешь получать только посты с мемами.'
+    bot.send_message(message.chat.id, text_message)
+
+
+@bot.message_handler(commands=['enable_memes'])
+def get_only_memes(message):
+    if memes_only[message.from_user.id]:
+        memes_only[message.from_user.id] = False
+        text_message = 'Режим мемов деактивирован!\n Теперь ты будешь получать случайные посты со всего Реддита.'
+    else:
+        memes_only[message.from_user.id] = True
+        text_message = 'Режим мемов активирован!\n Теперь ты будешь получать только посты с мемами.'
+    bot.send_message(message.chat.id, text_message)
+
+
+@bot.message_handler(commands=['add_memes'])
+def add_meme_to_list(message):
+    if message.from_user.id == 501248642:
+        if message.reply_to_message:
+            meme_subreddit = message.reply_to_message.caption.split('/r/')[1][:-1]
+            meme_subreddits.append(meme_subreddit)
+            bot.send_message(message.chat.id, "/r/{} успешно добавлен в список!".format(meme_subreddit))
+        else:
+            print(None)
+    else:
+        bot.send_message(message.chat.id,
+                         "Прости, {}, но у тебя нет права добавлять новые сабреддиты с мемами в список.".format(
+                             message.from_user.first_name))
 
 
 bot.polling(none_stop=True)
